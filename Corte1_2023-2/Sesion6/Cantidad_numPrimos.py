57175fd808f3e385a2ee524335c7aa84f18a0987
Maoest <110635545+Maoest@users.noreply.github.com>
2023-08-18 17:54:05 +0000

    starting

--- Corte1_2023-2/Sesion6/Cantidad_numPrimos.py
+++ Corte1_2023-2/Sesion6/Cantidad_numPrimos.py
@@ -1,11 +1,14 @@
 n=int(input('ingrese el numero de primos solicitada: '))
 con=1;x=2
-numero= print('1')
+numero= '1 '
 
-while con<=n:
-    for i in range(2,x):
+while con <n:
+    for i in range(2,x+1):
         primo=x%i
         if primo==0 and x==i:
-            print(x)
+            numero += str(f'{x} ') # imprmir de lado a lado y no hacoa abajo
             con+=1
+        elif primo==0 and x!=i:
+            break
     x+=1
+print(numero)
