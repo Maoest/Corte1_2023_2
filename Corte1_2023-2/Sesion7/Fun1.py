4ec28e8bf3a4910fd476ed83e4702e4ab78dc772
Maoest <110635545+Maoest@users.noreply.github.com>
2023-08-23 16:34:55 +0000

    sta

---
+++ Corte1_2023-2/Sesion7/Fun1.py
@@ -0,0 +1,12 @@
+# funcion con parametros y retorno
+
+def suma(n1,n2):
+    r=n1+n2
+    return r 
+
+print('inicio de programa')
+a=int(input('ingrese un numero: '))
+b=int(input('ingrese otro numero: '))
+res=suma(a,b)
+print(res)
+print('fin del programa')
