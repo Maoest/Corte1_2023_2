5d09c0dfd66fa79822935255e7c41b96d76916aa
Maoest <110635545+Maoest@users.noreply.github.com>
2023-09-03 21:11:28 +0000

    starting

--- Corte1_2023-2/Sesion7/TareaN-5/Modulo_vector.py
+++ Corte1_2023-2/Sesion7/TareaN-5/Modulo_vector.py
@@ -1,9 +1,17 @@
-a = int(input('Ingrese la cordenada x del origen del vector: '))
-b = int(input('Ingrese la cordenada y del origen del vector: '))
-c = int(input('Ingrese la cordenada x del fin del vector: '))
-d = int(input('Ingrese la cordenada y del fin del vector: '))
-x=c-a
-y=d-b
-m=(x**2 + y**2)**0.5
-print('El modulo del vector es: ',m)
-print(f'Las componentes en x y y son:{x,y}')
+   
+def Modulo():
+    a = int(input('Ingrese la cordenada x del origen del vector: '))
+    b = int(input('Ingrese la cordenada y del origen del vector: '))
+    c = int(input('Ingrese la cordenada x del fin del vector: '))
+    d = int(input('Ingrese la cordenada y del fin del vector: '))
+    x=c-a
+    y=d-b
+    m=(x**2 + y**2)**0.5
+    print('El modulo del vector es: ',m)
+    print(f'Las componentes en x y y son:{x,y}')
+
+def main():
+    Modulo()
+
+if __name__ == "__main__":
+    main()
