31ea9f80920fb8255b9a7e93660b9468d6e661e2
Maoest <110635545+Maoest@users.noreply.github.com>
2023-10-08 03:42:57 +0000

    starting

--- Corte2_2023-2/Sesion11/TaeaN-9_IMC.py
+++ Corte2_2023-2/Sesion11/TaeaN-9_IMC.py
@@ -1,28 +1,49 @@
 class Persona:
     def __init__(self):
-        self.nombre = None
-        self.apellido = None
-        self.altura = None
-        self.peso = None
+        self._nombre = None
+        self._apellido = None
+        self._altura = None
+        self._peso = None
+
+    def nombre(self):
+        return self._nombre
+    def nombre(self, value):
+        self._nombre = value
+
+    def apellido(self):
+        return self._apellido
+    def apellido(self, value):
+        self._apellido = value
+
+    def altura(self):
+        return self._altura
+    def altura(self, value):
+        self._altura = value
+
+    def peso(self):
+        return self._peso
+    def peso(self, value):
+        self._peso = value
 
     def imc(self):
-        a = round((self.peso / (self.altura/100) ** 2), 1)
-        if a <= 18.5:
-            categoria = 'Bajo'
-        elif a <= 24.9:
-            categoria = 'Saludable'
-        elif a <= 29.9:
-            categoria = 'Sobrepeso'
-        elif a <= 34.9:
-            categoria = 'Obesidad I'
-        elif a <= 39.9:
-            categoria = 'Obesidad II'
-        else:
-            categoria = 'Obesidad III'
-        
-        return f'IMC: {categoria}, {a}'
+        if self._altura is not None and self._peso is not None:
+            a = round((self._peso / (self._altura / 100) ** 2), 1)
+            if a <= 18.5:
+                categoria = 'Bajo'
+            elif a <= 24.9:
+                categoria = 'Saludable'
+            elif a <= 29.9:
+                categoria = 'Sobrepeso'
+            elif a <= 34.9:
+                categoria = 'Obesidad I'
+            elif a <= 39.9:
+                categoria = 'Obesidad II'
+            else:
+                categoria = 'Obesidad III'
 
-        
+            return f'IMC: {categoria}, {a}'
+        else:
+            return 'IMC no calculable'
 
 def main():
     personas = []
@@ -41,9 +62,9 @@ def main():
             print('Invalido')
 
     for i in personas:
-        print(f'\nNombre: {i.nombre} {i.apellido}\n'\
-              f'Altura: {i.altura}, Peso: {i.peso}\n'\
-                 f'IMC: {i.imc()}')
+        print(f'\nNombre: {i.nombre} {i.apellido}\n' \
+              f'Altura: {i.altura}, Peso: {i.peso}\n' \
+              f'IMC: {i.imc()}')
 
 if __name__ == "__main__":
     main()
