edf0e254e7adbdb979e13abd85e05b927aac153a
Maoest <110635545+Maoest@users.noreply.github.com>
2023-10-18 16:48:09 +0000

    starting

--- Corte2_2023-2/Sesion11/TaeaN-9_IMC.py
+++ Corte2_2023-2/Sesion11/TaeaN-9_IMC.py
@@ -5,25 +5,35 @@ class Persona:
         self._altura = None
         self._peso = None
 
-    def nombre(self):
-        return self._nombre
-    def nombre(self, value):
+    def set_nombre(self, value):
         self._nombre = value
 
-    def apellido(self):
-        return self._apellido
-    def apellido(self, value):
+    def get_nombre(self):
+        return self._nombre
+
+    def set_apellido(self, value):
         self._apellido = value
 
-    def altura(self):
-        return self._altura
-    def altura(self, value):
-        self._altura = value
+    def get_apellido(self):
+        return self._apellido
 
-    def peso(self):
+    def set_altura(self, value):
+        if 0 < value <= 300:
+            self._altura = value
+        else:
+            print("Altura no válida. Debe estar entre 0 y 300 cm.")
+
+    def get_altura(self):
+        return self._altura
+
+    def set_peso(self, value):
+        if 0 < value <= 500:
+            self._peso = value
+        else:
+            print("Peso no válido. Debe estar entre 0 y 500 kg.")
+
+    def get_peso(self):
         return self._peso
-    def peso(self, value):
-        self._peso = value
 
     def imc(self):
         if self._altura is not None and self._peso is not None:
@@ -50,10 +60,10 @@ def main():
     opc = 'n'
     while True:
         per = Persona()
-        per.nombre = input('Nombre: ')
-        per.apellido = input('Apellido: ')
-        per.altura = float(input('Altura (en cm): '))
-        per.peso = float(input('Peso (en kilogramos): '))
+        per.set_nombre(input('Nombre: '))
+        per.set_apellido(input('Apellido: '))
+        per.set_altura(float(input('Altura (en cm): ')))
+        per.set_peso(float(input('Peso (en kilogramos): ')))
         personas.append(per)
         opc = input('Desea salir? (y/n): ')
         if opc == 'y':
@@ -62,8 +72,8 @@ def main():
             print('Invalido')
 
     for i in personas:
-        print(f'\nNombre: {i.nombre} {i.apellido}\n' \
-              f'Altura: {i.altura}, Peso: {i.peso}\n' \
+        print(f'\nNombre: {i.get_nombre()} {i.get_apellido()}\n'
+              f'Altura: {i.get_altura()}, Peso: {i.get_peso()}\n'
               f'IMC: {i.imc()}')
 
 if __name__ == "__main__":
