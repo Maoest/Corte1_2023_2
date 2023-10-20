79a36d8e51befe3f7356bb2d0ca396ea467c8ccb
Maoest <110635545+Maoest@users.noreply.github.com>
2023-10-20 16:50:02 +0000

    starting

---
+++ Corte3_2023-2/Sesion15/Diagrama_UML.py
@@ -0,0 +1,19 @@
+# diagrama UML.
+# nombre clase
+#datos, +altura:int
+# si es privado, -altura:int                             foto todo, mejor diapositiva
+# constructor:
+# - __int__()
+# metodos: sets y gets
+# + setAltura():int
+
+
+
+
+
+
+
+
+
+
+
